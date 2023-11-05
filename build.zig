312f6b36363b1b6c342d409fdb2917e26afcb41c
Tristan Ross <tristan.ross@midstall.com>
2023-11-05 14:01:11 -0800

    feat: initial code

---
+++ build.zig
@@ -0,0 +1,46 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const no_docs = b.option(bool, "no-docs", "skip installing documentation") orelse false;
+
+    const fio = b.addModule("fio", .{
+        .source_file = .{ .path = b.pathFromRoot("fio.zig") },
+    });
+
+    const step_test = b.step("test", "Run all unit tests");
+
+    const unit_tests = b.addTest(.{
+        .root_source_file = .{
+            .path = b.pathFromRoot("fio.zig"),
+        },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+    step_test.dependOn(&run_unit_tests.step);
+
+    const exe_example = b.addExecutable(.{
+        .name = "example",
+        .root_source_file = .{
+            .path = b.pathFromRoot("example.zig"),
+        },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe_example.addModule("fio", fio);
+    b.installArtifact(exe_example);
+
+    if (!no_docs) {
+        const docs = b.addInstallDirectory(.{
+            .source_dir = unit_tests.getEmittedDocs(),
+            .install_dir = .prefix,
+            .install_subdir = "docs",
+        });
+
+        b.getInstallStep().dependOn(&docs.step);
+    }
+}
---
+++ example.zig
@@ -0,0 +1,6 @@
+const std = @import("std");
+const fio = @import("fio");
+
+pub fn main() void {
+    _ = fio;
+}
