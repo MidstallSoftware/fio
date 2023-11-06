46ff19586a63741641c87a6dc4d1c91eaffa9f14
Tristan Ross <tristan.ross@midstall.com>
2023-11-05 20:13:10 -0800

    feat(x86/linux): use direct port io and add ioperm

--- fio/io/x86/linux.zig
+++ fio/io/x86/linux.zig
@@ -1,28 +1,13 @@
-const builtin = @import("builtin");
 const std = @import("std");
 
-var iofd: ?std.fs.File = null;
+pub usingnamespace @import("freestanding.zig");
 
-fn init() std.fs.File.OpenError!void {
-    if (iofd == null) {
-        iofd = try std.fs.openFileAbsolute("/dev/port", .{
-            .mode = .read_write,
-        });
-    }
-}
-
-pub fn in(comptime T: type, port: u16) T {
-    init() catch |e| std.debug.panic("Failed to open /dev/io or /dev/iopl: {s}", .{@errorName(e)});
-
-    var buf: [@typeInfo(T).Int.bits]u8 = undefined;
-    _ = iofd.?.pread(buf, port) catch |e| std.debug.panic("Failed to read IO: {s}", .{@errorName(e)});
-    return std.mem.readInt(T, buf, builtin.cpu.arch.endian());
-}
-
-pub fn out(port: u16, data: anytype) void {
-    init() catch |e| std.debug.panic("Failed to open /dev/io or /dev/iopl: {s}", .{@errorName(e)});
-
-    var buf: [@typeInfo(@TypeOf(data)).Int.bits]u8 = undefined;
-    std.mem.writeInt(@TypeOf(data), buf, data, builtin.cpu.arch.endian());
-    _ = iofd.?.pwrite(buf, port) catch |e| std.debug.panic("Failed to write IO: {s}", .{@errorName(e)});
+pub fn ioperm(from: u32, num: u32, turn_on: bool) error{ InputOutput, OutOfMemory, PermissionDenied }!void {
+    return switch (std.os.errno(std.os.syscall3(101, from, num, @intFromBool(turn_on)))) {
+        .SUCCESS => void,
+        .IO => error.InputOutput,
+        .NOMEM => error.OutOfMemory,
+        .PERM => error.PermissionDenied,
+        else => |err| std.os.unexpectedErrno(err),
+    };
 }
