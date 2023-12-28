a31c415f0b7ba5cb7935b484195ee3d8512cb38c
Tristan Ross <tristan.ross@midstall.com>
2023-12-27 19:52:01 -0800

    fix(mem): make freestanding less dumb

--- fio/mem/freestanding.zig
+++ fio/mem/freestanding.zig
@@ -2,33 +2,12 @@ const builtin = @import("builtin");
 const std = @import("std");
 
 pub fn read(comptime T: type, addr: usize) T {
-    const ptr = @as(@Type(.{
-        .Pointer = .{
-            .size = .One,
-            .is_const = false,
-            .is_volatile = true,
-            .alignment = 1,
-            .address_space = .generic,
-            .child = T,
-            .is_allowzero = false,
-            .sentinel = null,
-        },
-    }), @ptrFromInt(addr));
+    const ptr: *const volatile T = @ptrFromInt(addr);
     return ptr.*;
 }
 
 pub fn write(addr: usize, data: anytype) void {
-    const ptr = @as(@Type(.{
-        .Pointer = .{
-            .size = .One,
-            .is_const = false,
-            .is_volatile = true,
-            .alignment = 1,
-            .address_space = .generic,
-            .child = @TypeOf(data),
-            .is_allowzero = false,
-            .sentinel = null,
-        },
-    }), @ptrFromInt(addr));
+    const T = @TypeOf(data);
+    const ptr: *const volatile T = @ptrFromInt(addr);
     ptr.* = data;
 }
